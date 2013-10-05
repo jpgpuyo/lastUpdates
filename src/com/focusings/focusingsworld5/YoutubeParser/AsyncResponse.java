a58cd82df7268ca19f9c021cb8076f95af0b6386
Albert Solé <pizzi202020@hotmail.com>
2013-10-06 01:18:50 +0200

    changing the package name

--- src/com/focusings/focusingsworld5/YoutubeParser/AsyncResponse.java
+++
@@ -1,9 +0,0 @@
-package com.focusings.focusingsworld5.YoutubeParser;
-
-import java.util.List;
-
-import com.focusings.focusingsworld5.ImageAndTextList.ImageAndText;
-
-public interface AsyncResponse {
-    void processFinish(List<ImageAndText> output,int tabNumber,boolean needToCallOnRefreshComplete);
-}
--- src/com/focusings/focusingsworld5/notificationManagement/AsyncNotificationResponse.java
+++
@@ -1,7 +0,0 @@
-package com.focusings.focusingsworld5.notificationManagement;
-
-import java.util.List;
-
-public interface AsyncNotificationResponse {
-    void sendNotification(List<Update> output);
-}
