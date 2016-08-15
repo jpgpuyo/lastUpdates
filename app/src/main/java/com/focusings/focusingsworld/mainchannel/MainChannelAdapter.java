8996a36838c90950372f8afe224c45d691f891b8
Joaquim Puyo <joaquim.puyo@worldline.com>
2016-08-15 12:09:26 +0200

    applied layout template from https://github.com/vitovalov/TabbedCoordinatorLayout

---
+++ app/src/main/java/com/focusings/focusingsworld/mainchannel/MainChannelAdapter.java
@@ -0,0 +1,50 @@
+package com.focusings.focusingsworld.mainchannel;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.focusings.focusingsworld.R;
+
+import java.util.List;
+
+public class MainChannelAdapter extends RecyclerView.Adapter<MainChannelAdapter.MyViewHolder> {
+
+    List<String> mListData;
+
+    public MainChannelAdapter(List<String> mListData) {
+        this.mListData = mListData;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item,
+                viewGroup, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder myViewHolder, int i) {
+        myViewHolder.title.setText(mListData.get(i));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mListData == null ? 0 : mListData.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+
+            title = (TextView) itemView.findViewById(R.id.listitem_name);
+        }
+    }
+
+}
+
