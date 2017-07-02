731d8b8afd9c713873111c36aa94b6e42ae2dd0e
Joaquim Puyo <joaquim.puyo@worldline.com>
2017-07-02 22:01:29 +0200

    domain layer: created subpackages for intteractors

--- app/src/main/java/com/focusings/focusingsworld/presentation/mainchannel/MainChannelModule.java
+++ app/src/main/java/com/focusings/focusingsworld/presentation/mainchannel/MainChannelModule.java
@@ -1,6 +1,6 @@
 package com.focusings.focusingsworld.presentation.mainchannel;
 
-import com.focusings.focusingsworld.domain.interactors.GetRecentVideosFromChannel;
+import com.focusings.focusingsworld.domain.interactors.recentvideos.GetRecentVideosFromChannel;
 import com.focusings.focusingsworld.domain.repository.YoutubeRepository;
 import com.focusings.focusingsworld.infrastructure.connectivity.Network;
 import com.focusings.focusingsworld.infrastructure.dagger.PerActivity;
