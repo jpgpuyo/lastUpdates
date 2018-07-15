508bb3a40710ebf31d6dda6dc53e4fa81432f1a8
jpgpuyo <jpgpuyo@gmail.com>
2018-07-15 20:16:42 +0200

    Show splash always at least one second

--- app/src/main/java/com/focusings/focusingsworld/features/splash/usecase/InitAppUseCase.java
+++ app/src/main/java/com/focusings/focusingsworld/features/splash/usecase/InitAppUseCase.java
@@ -1,5 +1,7 @@
 package com.focusings.focusingsworld.features.splash.usecase;
 
+import android.support.annotation.NonNull;
+
 import com.focusings.focusingsworld.core.executor.PostExecutionThread;
 import com.focusings.focusingsworld.core.executor.ThreadExecutor;
 import com.focusings.focusingsworld.core.interactor.UseCase;
@@ -8,6 +10,7 @@ import com.focusings.focusingsworld.data.youtube.models.YoutubeVideo;
 import com.focusings.focusingsworld.data.youtube.recentvideos.RecentVideosRepository;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 
@@ -25,6 +28,12 @@ public class InitAppUseCase extends UseCase<List<YoutubeVideo>, Void> {
 
     @Override
     protected Observable<List<YoutubeVideo>> buildUseCaseObservable(Void unused) {
-        return recentVideosRepository.getVideos(true, YoutubeChannel.FEATURED_CHANNEL_ID);
+        return recentVideosRepository.getVideos(true, YoutubeChannel.FEATURED_CHANNEL_ID)
+               .compose(listObservable -> applyDelayForShowSplasAtLeastOneSecond(listObservable));
+    }
+
+    @NonNull
+    private Observable<List<YoutubeVideo>> applyDelayForShowSplasAtLeastOneSecond(Observable<List<YoutubeVideo>> listObservable) {
+        return listObservable.delay(1, TimeUnit.SECONDS);
     }
 }
